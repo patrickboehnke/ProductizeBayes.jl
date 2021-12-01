c48cace8be417f7492f9d0e75eafc6b7d8b659d0
patrickboehnke <pboehnke@gmail.com>
2021-11-30 20:24:42 -0600

    try exporting the functions from StanSample

--- src/ProductizeBayes.jl
+++ src/ProductizeBayes.jl
@@ -5,5 +5,12 @@ import StanSample
 
 include("versionmodels/VersionedModel.jl")
 
-export VersionedModel
+export VersionedModel,
+        stan_sample,
+        read_samples,
+        read_summary,
+        stan_summary,
+        stan_generate_quantities,
+        read_generated_quantities,
+        diagnose
 end # module

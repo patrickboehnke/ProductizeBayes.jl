c1efa790fd62f2d5ddf8278da934ba8e53023c35
patrickboehnke <pboehnke@gmail.com>
2021-11-30 22:48:10 -0600

    removing a print

--- test/test_versioned_model/test_versioned_model.jl
+++ test/test_versioned_model/test_versioned_model.jl
@@ -20,7 +20,7 @@ data = Dict("N" => 10, "y" => [0, 1, 0, 1, 0, 0, 0, 0, 0, 1])
 
 sm = VersionedModel("bernoulli", bernoulli_model)
 rc = stan_sample(sm.stan_model; data, num_chains=2, seed=-1)
-println(sm.hash)
+
 @test sm.hash == "5dce4091b38059e657f64ee7222f2b74778d852c051ad33bf0d646d1f0f66a05237510dd556214a343f5da8981faa5aef866a3cb109897f6074adbd38c8835bd"
 
 if success(rc)

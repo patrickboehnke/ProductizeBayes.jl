e347a35fb04add8be0f2ba9b5ab9b0a7e9be8a7d
patrickboehnke <pboehnke@gmail.com>
2021-12-02 21:26:07 -0600

    laying groundwork for versioning

--- src/versionmodels/VersionedModel.jl
+++ src/versionmodels/VersionedModel.jl
@@ -1,18 +1,19 @@
 import Base: show
 using SHA
 using StanSample
+using HDF5
 
 
 mutable struct VersionedModel
     # Stan Model
-    stan_model::SampleModel;
+    stan_model::SampleModel
 
     # Versioning Detail Fields
-    hash::AbstractString;
-    #version_number::Int;
+    hash::AbstractString
+    version_number::Int;
 
     # Storage Information
-    #version_store_file::AbstractString;
+    version_store_file::AbstractString
 end
 
 function VersionedModel(name::AbstractString, model::AbstractString)

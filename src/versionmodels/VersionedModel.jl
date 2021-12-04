7358f6ac10244853f76968307fbe4ec0e8185cd2
patrickboehnke <pboehnke@gmail.com>
2021-12-03 22:46:27 -0600

    starting on the storage code

--- src/versionmodels/VersionedModel.jl
+++ src/versionmodels/VersionedModel.jl
@@ -10,15 +10,22 @@ mutable struct VersionedModel
 
     # Versioning Detail Fields
     hash::AbstractString
-    version_number::Int;
+    version_number::Int
 
     # Storage Information
     version_store_file::AbstractString
 end
 
-function VersionedModel(name::AbstractString, model::AbstractString)
+function VersionedModel(name::AbstractString, model::AbstractString, version_store_file::AbstractString)
     stan_model = SampleModel(name, model)
     hash = bytes2hex(sha3_512(lowercase(strip(model))))
+
+    fid = h5open(version_store_file, "cw")
+    if name in keys(fid)
+        versioned_model_data = fid[name]
+    else
+        versioned_model_data = create_group(fid, name)
+    end
 
     VersionedModel(stan_model, hash)
 

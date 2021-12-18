a70a3c9a552e3c4be62d40a8d63267acc086de7c
patrickboehnke <pboehnke@gmail.com>
2021-12-18 14:25:47 -0600

    Adding first example

---
+++ src/Examples/BernoulliExample.jl
@@ -0,0 +1,21 @@
+using ProductizeBayes
+
+
+bernoulli_model = "
+data {
+  int<lower=1> N;
+  int<lower=0,upper=1> y[N];
+}
+parameters {
+  real<lower=0,upper=1> theta;
+}
+model {
+  theta ~ beta(1,1);
+  y ~ bernoulli(theta);
+}
+";
+
+data = Dict("N" => 10, "y" => [0, 1, 0, 1, 0, 0, 0, 0, 0, 1])
+
+sm = VersionedModel("bernoulli", bernoulli_model)
+rc = stan_sample(sm.stan_model; data, num_chains=2, seed=-1)
